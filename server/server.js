90f4cfe183cee81034a8c88ac5593aa99cc55a85
radelmann <robadelmann@gmail.com>
2016-03-20 16:04:21 -0700

    basic node/express server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var env = require('./config/env.js');
+
+mongoose.connect(env.db_url);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+require('./routes.js')(app, passport);
+
+app.listen(env.port, function () {
+  console.log('listening on port: ' + env.port);
+});
