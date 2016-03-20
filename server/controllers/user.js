fc9ed571af1383d373fa0568a11535c130a731fb
radelmann <robadelmann@gmail.com>
2016-03-20 16:07:01 -0700

    user controller - login and register

---
+++ server/controllers/user.js
@@ -0,0 +1,79 @@
+//var crypto = require('crypto');
+var User = require('../models/user.js');
+var mail = require('../config/mail.js');
+var jwt = require('jwt-simple');
+
+module.exports = function(passport) {
+  return {
+    login: function(req, res, next) {
+      passport.authenticate('local-login', {
+        session: false
+      }, function(err, user, info) {
+        if (err) {
+          return next(err)
+        }
+
+        if (!user) {
+          res.status(401); 
+          res.json({message: info});
+        }
+
+        //user has authenticated correctly thus we create a JWT token 
+        //encode user id as token
+        var token = jwt.encode({id: user._id}, 'secret');
+        res.status(200);
+        res.json({token: token});
+
+      })(req, res, next);
+    },
+
+    isAuth: function(req, res, next) {
+      // checking to see if the user is authenticated
+      // grab the token in the header, if any
+      // then decode the token, which we end up being the user object
+      // check to see if that user exists in the database
+      var token = req.headers['x-access-token'];
+      if (!token) {
+        next(new Error('No token'));
+      } else {
+        var user_Id = jwt.decode(token, 'secret');
+        //decode userid as token, attempt to find user
+        User.findOne({
+            _id: user_Id
+          })
+          .then(function(foundUser) {
+            if (foundUser) {
+              res.send(200);
+            } else {
+              res.send(401);
+            }
+          })
+          .fail(function(error) {
+            next(error);
+          });
+      }
+    },
+
+    register: function(req, res, next) {
+      passport.authenticate('local-register', {
+        session: false
+      }, function(err, user, info) {
+        if (err) {
+          return next(err)
+        }
+
+        if (!user) {
+          res.status(401); 
+          res.json({message: info});
+        }
+
+        //user has authenticated correctly thus we create a JWT token 
+        //encode user id as token
+        var token = jwt.encode({id: user._id}, 'secret');
+        res.status(200);
+        res.json({token: token});
+
+      })(req, res, next);  
+    }
+  }
+}
