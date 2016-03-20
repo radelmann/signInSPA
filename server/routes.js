5b9dd629e54637f35fdb2b7741e5b3d88a653dc9
radelmann <robadelmann@gmail.com>
2016-03-20 16:09:24 -0700

    routes - cleanup

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,5 @@
 var sanitize = require('mongo-sanitize');
 
-var isAuth = function(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
-
-  res.redirect('/');
-}
-
 var cleanInput = function(req, res, next) {
   req.params = sanitize(req.params);
   req.body = sanitize(req.body);
@@ -16,11 +9,7 @@ var cleanInput = function(req, res, next) {
 module.exports = function(app, passport) {
   var user = require('./controllers/user.js')(passport);
 
-  app.post('/register', cleanInput, user.register);
+  app.post('/register', cleanInput, ,user.register);
 
   app.post('/login', cleanInput, user.login);
-
-  // app.post('/login', cleanInput, passport.authenticate('local-login', {
-  //   sessioin:false
-  // }),user.login);
 }
