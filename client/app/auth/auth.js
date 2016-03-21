732dfdb6285c805db73852f09451708eb967abb1
radelmann <robadelmann@gmail.com>
2016-03-21 15:24:26 -0700

    UI css and user feedback messages

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -3,6 +3,9 @@ angular.module('signin.auth', [])
 .controller('AuthController', function($scope, $window, $location, $routeParams, Auth) {
   $scope.user = {};
 
+  $scope.info = '';
+  $scope.error = '';
+
   $scope.login = function() {
     Auth.login($scope.user)
       .then(function(token) {
@@ -10,38 +13,40 @@ angular.module('signin.auth', [])
         $location.path('/account');
       })
       .catch(function(error) {
-        console.error(error);
+        $scope.error = error.data.message;
       });
   };
 
   $scope.register = function() {
+    //todo field validation
+    
     Auth.register($scope.user)
       .then(function(token) {
         $window.localStorage.setItem('com.signin', token);
         $location.path('/account');
       })
       .catch(function(error) {
-        console.error(error);
+        $scope.error = error.data.message;
       });
   };
 
   $scope.forgot = function() {
     Auth.forgot($scope.user)
-      .then(function(data) {
-        //todo display message
+      .then(function(message) {
+        $scope.info = message;
       })
       .catch(function(error) {
-        console.error(error);
+        $scope.error = error.data.message;
       });
   };
 
   $scope.reset = function() {
     Auth.reset($scope.user, $routeParams.token)
-      .then(function(data) {
-        //todo display message
+      .then(function(message) {
+        $scope.info = data.message;
       })
       .catch(function(error) {
-        console.error(error);
+        $scope.error = error.data.message;
       });
   };
 });
