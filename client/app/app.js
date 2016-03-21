6a4db26defc20aa96b2028360f5b1b1e58b8d72b
radelmann <robadelmann@gmail.com>
2016-03-21 10:52:04 -0700

    added gulp file for live reloading

--- client/app/app.js
+++ client/app/app.js
@@ -1,31 +1,34 @@
 angular.module('signin', [
     'signin.services',
-    //'signin.account',
     'signin.auth',
-    'ngRoute',
+    'ngRoute', 
     'ui.router'
   ])
-  
-  .config(function ($routeProvider, $stateProvider, $sceProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/signin')
+  .config(function ($httpProvider,$routeProvider, $stateProvider, $sceProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/login')
     $stateProvider
+    .state('home', {
+        url:'/',
+        templateUrl: 'app/auth/login.html',
+        controller: 'AuthController'
+      })
       .state('login', {
         url:'/login',
         templateUrl: 'app/auth/login.html',
         controller: 'AuthController'
       })
-      .state('/register', {
+      .state('register', {
         url:'register',
         templateUrl: 'app/auth/register.html',
         controller: 'AuthController'
       })
-      .state('/forgot', {
-        url:'forgot',
+      .state('forgot', {
+        url:'/forgot',
         templateUrl: 'app/auth/forgot.html',
         controller: 'AuthController'
       })
-      .state('/reset', {
-        url:'reset/:token',
+      .state('reset', {
+        url:'/reset/:token',
         templateUrl: 'app/auth/reset.html',
         controller: 'AuthController'
       })
@@ -34,26 +37,26 @@ angular.module('signin', [
       //   controller: 'AccountController'
       // })
     
-    $httpProvider.interceptors.push('AttachTokens');
-  })
-  //todo state and route provider
-  .factory('AttachTokens', function ($window) {
-    var attach = {
-      request: function (object) {
-        var jwt = $window.localStorage.getItem('com.signin');
-        if (jwt) {
-          object.headers['x-access-token'] = jwt;
-        }
-        object.headers['Allow-Control-Allow-Origin'] = '*';
-        return object;
-      }
-    };
-    return attach;
-  })
-  .run(function ($rootScope, $location, Auth) {
-    $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-      if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-        $location.path('/login');
-      }
-    });
+    // $httpProvider.interceptors.push('AttachTokens');
   });
+  //todo state and route provider
+  // .factory('AttachTokens', function ($window) {
+  //   var attach = {
+  //     request: function (object) {
+  //       var jwt = $window.localStorage.getItem('com.signin');
+  //       if (jwt) {
+  //         object.headers['x-access-token'] = jwt;
+  //       }
+  //       object.headers['Allow-Control-Allow-Origin'] = '*';
+  //       return object;
+  //     }
+  //   };
+  //   return attach;
+  // })
+  // .run(function ($rootScope, $location, Auth) {
+  //   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+  //     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+  //       $location.path('/login');
+  //     }
+  //   });
+  // });
